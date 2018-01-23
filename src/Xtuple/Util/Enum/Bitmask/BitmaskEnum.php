fab024fc7cf9910ecf80536d346df4f62b7edb83
Andrey Mikheychik <a.mikheychik@gmail.com>
2018-01-23 06:33:40 -0500

    BitmaskEnum added

---
+++ src/Xtuple/Util/Enum/Bitmask/BitmaskEnum.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace Xtuple\Util\Enum\Bitmask;
+
+abstract class BitmaskEnum {
+  /** @var int */
+  private $value;
+
+  /**
+   * @throws \InvalidArgumentException
+   *
+   * @param int $value
+   */
+  public function __construct(int $value) {
+    if ($value > self::max()) {
+      throw new \InvalidArgumentException(strtr('Value `{value}` is not supported in bitmask enum {class}', [
+        '{value}' => $value,
+        '{class}' => static::class,
+      ]));
+    }
+    $this->value = $value;
+  }
+
+  public final function value(): int {
+    return $this->value;
+  }
+
+  public final function is(int $value): bool {
+    return $this->value === $value;
+  }
+
+  /** @var int[] */
+  private static $max;
+
+  private static function max(): int {
+    if (!isset(self::$max[static::class])) {
+      $bitmask = 0b0;
+      foreach ((new \ReflectionClass(static::class))->getConstants() as $constant => $mask) {
+        $bitmask |= $mask;
+      }
+      $size = log($bitmask + 1, 2);
+      $max = (int) pow(2, ceil($size)) - 1;
+      if (ceil($size) !== floor($size)) {
+        throw new \LogicException(strtr('Bitmask {class} values do not form full {value} (0b{bin}) mask', [
+          '{class}' => static::class,
+          '{value}' => $max,
+          '{bin}' => decbin($max),
+        ]));
+      }
+      self::$max[static::class] = $max;
+    }
+    return self::$max[static::class];
+  }
+}
---
+++ tests/Xtuple/Util/Enum/Bitmask/BitmaskEnumTest.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+namespace Xtuple\Util\Enum\Bitmask;
+
+use PHPUnit\Framework\TestCase;
+
+class BitmaskEnumTest
+  extends TestCase {
+  /**
+   * @expectedException \LogicException
+   * @expectedExceptionMessage Bitmask Xtuple\Util\Enum\Bitmask\IncorrectBitmask values do not form full 15 (0b1111)
+   *                           mask
+   */
+  public function testValues() {
+    new CorrectBitmask(CorrectBitmask::ZERO);
+    $bitmask = new CorrectBitmask(CorrectBitmask::ZERO | CorrectBitmask::FOUR | CorrectBitmask::EIGHT);
+    self::assertEquals(0b1100, $bitmask->value());
+    $bitmask = new CorrectBitmask(CorrectBitmask::TWO | CorrectBitmask::FOUR | CorrectBitmask::EIGHT);
+    self::assertTrue($bitmask->is(0b1110));
+    new IncorrectBitmask(IncorrectBitmask::ZERO);
+  }
+
+  /**
+   * @expectedException \InvalidArgumentException
+   * @expectedExceptionMessage Value `16` is not supported in bitmask enum Xtuple\Util\Enum\Bitmask\SplitBitmask
+   */
+  public function testConstructor() {
+    new SplitBitmask(SplitBitmask::ONE | SplitBitmask::THREE | SplitBitmask::TWELVE | SplitBitmask::FIFTEEN);
+    new SplitBitmask(0b10000);
+  }
+}
+
+final class CorrectBitmask
+  extends BitmaskEnum {
+  public const ZERO = 0b0;
+  public const ONE = 0b0001;
+  public const TWO = 0b0010;
+  public const FOUR = 0b0100;
+  public const EIGHT = 0b1000;
+}
+
+final class IncorrectBitmask
+  extends BitmaskEnum {
+  public const ZERO = 0b0;
+  public const ONE = 0b0001;
+  public const FOUR = 0b0100;
+  public const EIGHT = 0b1000;
+}
+
+final class SplitBitmask
+  extends BitmaskEnum {
+  public const ONE = 0b0001;
+  public const THREE = 0b0011;
+  public const TWELVE = 0b1100;
+  public const FIFTEEN = 0b1000;
+}
